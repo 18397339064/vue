42897148f656ed4aa2972fff8ed58a97a6d3946c
15873629093 <2093139643@qq.com>
2020-12-15 16:33:00 +0800

    staff

--- src/main.js
+++ src/main.js
@@ -1,21 +1,7 @@
 import Vue from 'vue'
-import Backstage from './components/backstage/backstage.vue'
-import StaffLogin from './components/backstage/staffLogin.vue'
-import NavMenu from "./components/front/NavMenu";
-
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.use(ElementUI)
-
-// 引用axios，并设置基础URL为后端服务api地址
-import Axios from 'axios'
-
-Axios.defaults.baseURL = "http://localhost:8080/ssm_web"
-// 将baseURL绑定到全局
-Vue.prototype.$axios = Axios
+import App from './App.vue'
 
 new Vue({
   el: '#app',
-  render: h => h(NavMenu)
+  render: h => h(App)
 })
