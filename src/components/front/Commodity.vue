aa7d04655fa711dc33e687334f5014ddd1b5e243
15292147228 <3064656416@qq.com>
2020-12-23 14:41:54 +0800

    ljl

--- src/components/front/Commodity.vue
+++ src/components/front/Commodity.vue
@@ -1,26 +1,25 @@
 <template>
     <div id="app">
       <br>
-      <el-page-header content="商品详情">
+      <el-page-header content="商品详情" @back="goBack">
       </el-page-header>
       <br>
       <hr>
       <div style="margin-left: 100px">
         <br>
         <br>
-        <el-popover placement="right-start" trigger="hover">
-          <img :src="img1" style="width: 700px">
-          <img slot="reference" :src="img1">
+        <el-popover>
+          <img slot="reference" :src="commodity.comimg" height="400px" width="400px">
         </el-popover>
         <div style="margin-left: 450px;margin-top: -410px">
-          <h2>网红烘焙蛋糕 好吃的蛋糕</h2>
-          <h4>价格&nbsp;&nbsp;&nbsp;<span style="font-size: 20px;color: red;">￥18</span></h4>
-          <h4>总销售&nbsp;&nbsp;&nbsp;<span style="font-size: 20px;color: red;">20000</span></h4>
-          <h4>产地&nbsp;&nbsp;&nbsp;<span>中国</span></h4>
-          <h4>规格&nbsp;&nbsp;&nbsp;<span>50g</span></h4>
+          <h2>{{commodity.comname}}</h2>
+          <h4>价格&nbsp;&nbsp;&nbsp;<span style="font-size: 20px;color: red;">{{commodity.comprice}}</span></h4>
+          <h4>总销售&nbsp;&nbsp;&nbsp;<span style="font-size: 20px;color: red;">{{commodity.comnum}}</span></h4>
+          <h4>产地&nbsp;&nbsp;&nbsp;<span>{{commodity.complace}}</span></h4>
+          <h4>规格&nbsp;&nbsp;&nbsp;<span>{{commodity.comsperifications}}</span></h4>
           <h5>数量&nbsp;&nbsp;&nbsp;<el-input-number v-model="count" :min="1" :max="100"></el-input-number></h5>
           <br>
-          <el-button type="danger">加入购物车</el-button>
+          <el-button type="danger" @click="jiaRu(commodity)">加入购物车</el-button>
         </div>
       </div>
       <br>
@@ -38,11 +37,48 @@
         data(){
           return {
             img1:"../img/11.png",
-            count:1
+            count:1,
+            commodity:{},
+            id:""
           }
         },
         methods:{
+            getDate(){
+              var _this = this;
+              _this.id = this.$route.params.id;
+              var params = new URLSearchParams();
+              params.append("id",_this.id);
+              this.$axios.post("queryById.action",params).then(function (result) {  //成功  执行then里面的方法
+                _this.commodity = result.data;
+                console.log(_this.commodity)
+              }).catch(function (error) { //失败 执行catch方法
+                console.log(error)
+              });
+            },
+            goBack(){
+              this.$router.go(-1);
+            },
+            jiaRu(commodity){
+              var _this = this;
+              var params = new URLSearchParams();
+              params.append("userid",sessionStorage.getItem("userid"));
+              params.append("comid",commodity.comid);
+              params.append("shopCount",_this.count);
+              this.$axios.post("addShoppingCar.action",params).then(function (result) {  //成功  执行then里面的方法
+                _this.$message({
+                  showClose: true,
+                  message: result.data,
+                  type: 'success'
+                })
+                _this.$router.go(-1);
 
+              }).catch(function (error) { //失败 执行catch方法
+                console.log(error)
+              });
+            }
+        },
+        created() {
+        this.getDate();
         }
     }
 </script>
--- src/router/routerconfig.js
+++ src/router/routerconfig.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import StaffLogin from '../components/backstage/staffLogin.vue'
 import Backstage from '../components/backstage/backstage.vue'
+import Commodity from '../components/front/Commodity.vue'
+import NavMenu from "../components/front/NavMenu";
 
 //使用vue-router组件
 Vue.use(VueRouter)
@@ -12,13 +14,19 @@ Vue.use(VueRouter)
 var routes=[
 	{path:"/stafflogin", component:StaffLogin},
   {path:"/backstage",component:Backstage},
-  {path:"/",component:StaffLogin}
+  {path:"/commodity",name:"Commodity",component: Commodity},
+  {path:"/",component:NavMenu}
 ]
 
 //将路由数组应用到路由管理器中
 var router=new VueRouter({
 	routes:routes
 })
+
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 //导出路由管理器对象
 export default router
